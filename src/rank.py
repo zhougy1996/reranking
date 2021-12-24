716dd5f418e90d81d620d7cb6fe0075e2a508bc9
zhougy <zhougy1996@outlook.com>
2021-12-24 15:05:45 +0800

    Solve path problem

--- src/rank.py
+++ src/rank.py
@@ -1,3 +1,5 @@
+import os
+
 import torch
 import pandas as pd
 import numpy
@@ -6,11 +8,15 @@ from transformers import BertTokenizer
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 MAX_TEXT_LENGTH = 256
 
-bert_path = '../models/pretrained/bert-base-uncased'
-# Init tokenizer
-tokenizer = BertTokenizer.from_pretrained(bert_path)
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.join(CURRENT_DIR, '..')
+BERT_DIR = os.path.join(ROOT_DIR, 'models/pretrained/bert-base-uncased')
+CURRENT_MODEL_FILE = os.path.join(ROOT_DIR, 'models/my_model.pkl')
 
-model = torch.load('../models/my_model.pkl', map_location=DEVICE)
+# Init tokenizer
+tokenizer = BertTokenizer.from_pretrained(BERT_DIR)
+
+model = torch.load(CURRENT_MODEL_FILE, map_location=DEVICE)
 model.eval()
 
 
@@ -31,13 +37,13 @@ def get_score(query, passage):
 
 # 读取验证集（使用2019年的测试集作为验证集，43个验证查询）
 # val_set
-val_set = pd.read_csv('../data/msmarco-passagetest2019-43-top1000.tsv', sep='\t', header=None)
+val_set = pd.read_csv(os.path.join(ROOT_DIR, 'data/msmarco-passagetest2019-43-top1000.tsv'), sep='\t', header=None)
 val_set.columns = ['qid', 'pid', 'query', 'passage']
 # print(val_set['qid'].value_counts().count())
 
 # 读取测试集（2020年的测试集，54个测试查询）
 # test_set
-test_set = pd.read_csv('../data/msmarco-passagetest2020-54-top1000.tsv', sep='\t', header=None)
+test_set = pd.read_csv(os.path.join(ROOT_DIR, 'data/msmarco-passagetest2020-54-top1000.tsv'), sep='\t', header=None)
 test_set.columns = ['qid', 'pid', 'query', 'passage']
 # print(test_set['qid'].value_counts().count())
 
--- src/run.py
+++ src/run.py
@@ -1,22 +1,26 @@
+import os
+
 import torch
 import pandas as pd
 from transformers import BertTokenizer
 from models import TextClassifier
 from trainer import Trainer
 
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-bert_path = '../models/pretrained/bert-base-uncased'
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.join(CURRENT_DIR, '..')
+BERT_DIR = os.path.join(ROOT_DIR, 'models/pretrained/bert-base-uncased')
 
 # Init tokenizer
-tokenizer = BertTokenizer.from_pretrained(bert_path)
+tokenizer = BertTokenizer.from_pretrained(BERT_DIR)
 # Init model
-model = TextClassifier(bert_path, 2).to(DEVICE)
+model = TextClassifier(BERT_DIR, 2).to(DEVICE)
 # Init dataset
-dataset = pd.read_csv("../data/train_set.tsv", sep="\t")
+dataset = pd.read_csv(os.path.join(ROOT_DIR, 'data/train_set.tsv'), sep='\t')
 # Init trainer
 trainer = Trainer(model, tokenizer, dataset)
 
-
-trainer.train(DEVICE, 1)
+# Train with device and epoch_num
+trainer.train(DEVICE, 2)
 # Save trained model
-torch.save(model, '../models/my_model.pkl')
+torch.save(model, os.path.join(ROOT_DIR, 'models/my_model.pkl'))
